cd9a9e073c655024aa84612ff0ff0692d281f8b5
pilshikovdenis <denisnaga169@gmail.com>
2022-11-22 22:12:57 +0300

    dao, entity, service classes was cloned from spring_mysql prject

---
+++ src/main/java/com/pilshikov/spring/rest/controller/MyRestController.java
@@ -0,0 +1,7 @@
+package com.pilshikov.spring.rest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+}
---
+++ src/main/java/com/pilshikov/spring/rest/dao/EmployeeDAO.java
@@ -0,0 +1,13 @@
+package com.pilshikov.spring.rest.dao;
+
+import com.pilshikov.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+    public void saveEmployee(Employee employee);
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
