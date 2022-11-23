6d4327790908e2c7cc3fab55a5067e4e7591ac73
pilshikovdenis <denisnaga169@gmail.com>
2022-11-23 10:54:25 +0300

    adding new employee functional was added

--- src/main/java/com/pilshikov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/pilshikov/spring/rest/controller/MyRestController.java
@@ -33,6 +33,11 @@ public class MyRestController {
         return employee;
     }
 
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
 
 
 }
